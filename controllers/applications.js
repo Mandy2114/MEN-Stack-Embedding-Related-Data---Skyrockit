ed8c3288314c2331b834b7def43945b59233fa0b
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-01 12:23:28 -0400

    add application#2

--- controllers/applications.js
+++ controllers/applications.js
@@ -6,5 +6,18 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 //building out router logic here
+// controllers/applications.js
+// router.get('/', (req, res) => {
+//   res.send('Hello applications index route!');
+// });
+// controllers/applications.js
+router.get('/', async (req, res) => {
+  try {
+    res.render('applications/index.ejs');
+  } catch (error) {
+    console.log(error)
+    res.redirect('/')
+  }
+});
 
 module.exports = router;
